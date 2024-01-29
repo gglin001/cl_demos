bab5abbc0b09f7cb6e1b8c536190af08be3b85f6
Allen Guo <guosonglin001@gmail.com>
2024-01-29 14:50:10 +0800

    add .cmake-format.py

---
+++ .cmake-format.py
@@ -0,0 +1,15 @@
+# -----------------------------
+# Options affecting formatting.
+# -----------------------------
+with section("format"):
+
+    # How wide to allow formatted cmake files
+    line_width = 80
+
+# ------------------------------------------------
+# Options affecting comment reflow and formatting.
+# ------------------------------------------------
+with section("markup"):
+
+    # enable comment markup parsing and reflow
+    enable_markup = False
