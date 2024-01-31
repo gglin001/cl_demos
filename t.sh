346b6ce3ecf747f24da98c6d2e3ebc46ec75c06a
Allen Guo <guosonglin001@gmail.com>
2024-01-31 14:24:12 +0800

    clean

--- t.sh
+++ t.sh
@@ -3,4 +3,4 @@ micromamba install -y pocl
 # for mac or windows
 micromamba install -y khronos-opencl-icd-loader clhpp
 # for linux
-micromamba install -y ocl-icd ocl-icd clhpp
+micromamba install -y ocl-icd clhpp
