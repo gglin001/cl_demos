14ccb7a01ef3c92b7f7847b84293b1ae820bb6e0
Allen Guo <guosonglin001@gmail.com>
2024-03-27 08:50:59 +0000

    wip

--- csrc/absf/main.cpp
+++ csrc/absf/main.cpp
@@ -15,13 +15,15 @@
   } while (0)
 
 static const size_t buffer_size = 4 * sizeof(float);
-static const char *kerenel_name = "absf_wrap";
+static const char *kerenel_name = "absf";
 
 static const char *program_source = R"(
 kernel void
-absf_wrap (global const float *in,
+absf (global const float *in,
       global float *out)
 {
+  printf("gid: %d, lid: %d, gid: %d \n",
+    get_global_id(0), get_local_id(0), get_group_id(0));
   size_t gid = get_global_id(0);
   out[gid] = in[gid] * -1;
 }
